9736954492ea35bbdedb54fb3a1346ca762aefd6
Victor Barros <victorwbarros@gmail.com>
2023-11-18 11:18:09 -0300

    feat: create surfers repository

---
+++ app/Http/Repositories/SurferRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Models\Surfer;
+
+class SurferRepository
+{
+
+    public function create($newSurfer)
+    {
+        $surfer = Surfer::create($newSurfer);
+        return $surfer;
+    }
+
+    public function find($number)
+    {
+        return Surfer::where('number', $number)->first();
+    }
+}
