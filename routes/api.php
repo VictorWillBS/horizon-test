83f7edd62d7f8d36894b9a442a92899cc4e6d5e0
Victor Barros <victorwbarros@gmail.com>
2023-11-18 13:15:37 -0300

    Feat: add create battery route .

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BatteryController;
 use App\Http\Controllers\Surfer\SurferController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,5 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/surfer', [SurferController::class, 'create'])->name('surfer.create');
-Route::get('/surfer', [SurferController::class, 'getAll'])->name('surfer.getAll');
+Route::prefix('/surfer')->controller(SurferController::class)->group(function () {
+    Route::get('/', 'getAll')->name('surfer.getAll');
+    Route::post('/', 'create')->name('surfer.create');
+});
+
+Route::prefix('/batery')->controller(BatteryController::class)->group(function () {
+    Route::post('/', 'create')->name('battery.create');
+});
