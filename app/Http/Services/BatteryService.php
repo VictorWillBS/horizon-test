44127ca6d0a57e113052d0e981f21134b600c2fa
Victor Barros <victorwbarros@gmail.com>
2023-11-18 13:13:21 -0300

    feat: add create battery function in service

--- app/Http/Services/BatteryService.php
+++ app/Http/Services/BatteryService.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 
 class BatteryService
 {
+    private BatteryRepository $batteryRepository;
+    public function __construct(BatteryRepository $batteryRepository)
+    {
+        $this->batteryRepository = $batteryRepository;
+    }
+
+    public function create(array $req)
+    {
+        $newBattery = $this->batteryRepository->create($req);
+        return  $newBattery;
+    }
 }
