0f39a694879dac0419c1adacffe25c538118c284
VictorWillBS <victorwbarros@gmail.com>
2023-11-21 16:36:58 -0300

    feat: create get battery waves repositories

--- app/Http/Repositories/BatteryRepository.php
+++ app/Http/Repositories/BatteryRepository.php
@@ -3,12 +3,21 @@
 namespace App\Http\Repositories;
 
 use App\Models\Battery;
+use App\Models\Notes;
+use App\Models\Wave;
+use Illuminate\Support\Facades\DB;
 
 class BatteryRepository
 {
+
     public function create(array $newBattery)
     {
         $battery = Battery::create($newBattery["participants_numbers"]);
         return $battery;
+    }
+    public function getBatteryWaves(int $id)
+    {
+        $waves = Wave::where("battery_id", $id)->with(["notes", "surfer"])->get();
+        return $waves;
     }
 }
