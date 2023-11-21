7efb86ffb87f6ce8abe081db3d1e4934a642098b
VictorWillBS <victorwbarros@gmail.com>
2023-11-21 16:38:23 -0300

    feat: create waves and notes relationship model

--- app/Models/Wave.php
+++ app/Models/Wave.php
@@ -10,4 +10,9 @@ class Wave extends Model
     use HasFactory;
 
     protected $fillable = ["surfer_number", "battery_id"];
+
+    public function notes()
+    {
+        return $this->hasOne(Notes::class, "wave_id", "id");
+    }
 }
