30cf127a41ef70efc4ae67808bdb6f7d50b062f2
VictorWillBS <victorwbarros@gmail.com>
2023-11-21 00:53:24 -0300

    feat: create notes table migration

---
+++ database/migrations/2023_11_21_025856_create_notes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->integer("note_1");
+            $table->integer("note_2");
+            $table->integer("note_3");
+            $table->foreignId("wave_id")->references("id")->on("waves");
+            $table->unique("wave_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
